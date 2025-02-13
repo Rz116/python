143922ad408509a13175de76df438e6f9c17e092
Rz116 <147172188+Rz116@users.noreply.github.com>
2025-02-13 17:56:15 -0500

    Update tutorial_1.py

--- tutorial_1.py
+++ tutorial_1.py
@@ -5,14 +5,15 @@ app = Flask(__name__)
 
 def main():
     if request.method == "GET":
-        print("hello")
         return render_template("index1.html")
     else:
         Getinfo()
         return render_template("index1.html")
+    
 def Getinfo():
     name = request.form.get("Nameinput")
     email = request.form.get("MailInput")
-    print("Hello" + name + "Thank you for your email" + email)
+    print("Hello " + name + " Thank you for your email: " + email)
+    
 if __name__ == "__main__":
     app.run()
