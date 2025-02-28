d93f64f63f18d9ddcdb679f02d545892d2b6293d
Rz116 <147172188+Rz116@users.noreply.github.com>
2025-02-28 10:09:12 -0500

    Update tutorial_1.py

--- tutorial_1.py
+++ tutorial_1.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request,redirect,url_for
 import os.path
 from os import path
+
 app = Flask(__name__)
 @app.route('/',methods = ["GET","POST"])
 
@@ -9,22 +10,26 @@ def main():
         return render_template("index1.html")
     else:
         info()
-        return render_template("index2.html")
+        return render_template("index1.html")
     
 @app.route("/info",methods = ["POST"])    
 def info():
     global name,email
     name = request.form.get("Nameinput")
     email = request.form.get("passwordInput")
-    FileSave()
-    return render_template('index2.html',username=name,password=email)
+    
+    if (name == "" or email == ""):
+        return render_template('index1.html',error = "You need to type in both a password and Username")
+        return render_template('index1.html',username = "",password = "")
+    else:
+        FileSave()
+        return render_template('index2.html',username=name,password=email)
 
 def FileSave():
-    filename = "Information" + ".doc"
+    filename = name + ".doc"
     filedir = os.path.dirname(os.path.realpath("__file__"))
     existing = bool(path.exists(filename))
     if(existing == False):
-        adminfile = open(filename, "x")
         adminfile = open(filename, "w")
         adminfile.write("Username: " + name + " Password: " + email + "\n")
         adminfile.close()
