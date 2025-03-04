c9a74d618110b6500509e6acac5f959904f66082
Rz116 <147172188+Rz116@users.noreply.github.com>
2025-03-03 20:58:41 -0500

    Update tutorial_1.py

--- tutorial_1.py
+++ tutorial_1.py
@@ -1,46 +1,60 @@
-from flask import Flask,render_template,request,redirect,url_for
 import os.path
 from os import path
 
-app = Flask(__name__)
-@app.route('/',methods = ["GET","POST"])
-
 def main():
-    if request.method == "GET":
-        return render_template("index1.html")
-    else:
-        info()
-        return render_template("index1.html")
-    
-@app.route("/info",methods = ["POST"])    
-def info():
-    global name,email
-    name = request.form.get("Nameinput")
-    email = request.form.get("passwordInput")
-    
-    if (name == "" or email == ""):
-        return render_template('index1.html',error = "You need to type in both a password and Username")
-        return render_template('index1.html',username = "",password = "")
-    else:
-        FileSave()
-        return render_template('index2.html',username=name,password=email)
-
-def FileSave():
-    filename = name + ".doc"
     filedir = os.path.dirname(os.path.realpath("__file__"))
-    existing = bool(path.exists(filename))
-    if(existing == False):
-        adminfile = open(filename, "x")
-        adminfile.close()
-        write()
-    else:
-        adminfile = open(filename,"a")
-        adminfile.write("Username: " + name + " Password: " + email + "\n")
-        
-def write():
-    adminfile = open(filename, "w")
-    adminfile.write("Username: " + name + " Password: " + email + "\n")
-    adminfile.close()
-if __name__ == "__main__":
-    app.run()
+    whichone = str(input("Which Deparment would you like to shop in: \n "
+                                             "1.Fruit \n"
+                                             "2.Poultry \n"
+                                             "3.Meat \n"
+                                             "4.Beverages \n"
+                                             "5.Frozen Foods \n"
+                                             "6.Dietry Food \n"
+                                             "7.Kosher \n"
+                                             "8. Halal \n"
+                                             "9. Cart \n"
+                                             "Type in a number from 1-8 to select an option: "))
+                                                                   
+    match(whichone):
+        case "1":
+            filepath = filedir + "\Fruit.py"
+            
+        case "2":
+            filepath = filedir + "\Poultry.py"
+            
+        case "3":
+            filepath = filedir + "\Meat.py"
+            
+        case "4":
+            filepath = filedir  + "\Beverages.py"
+            
+        case "5":
+            filepath = filedir + "\Frozen.py"
+            
+        case "6":
+            filepath = filedir  + "\Dietry.py"
+            
+        case"7":
+            filepath = filedir + "\Kosher.py"
+            
+        case "8":
+            filepath = filedir  + "\halal.py"
+            
+        case "9":
+            filepath = filedir + "\cart.py"
+            
+        case default:
+            print("BIG BIG ISSUE BROTHER")
+            main()
+    
+    filenamepath = {
+            "__file__":filepath,
+            "__name__":"__main__",
+            }
 
+    with open(filepath,"rb") as file:
+        exec(compile(file.read(),filepath,"exec"),filenamepath);
+    main()
+    
+if __name__ == "__main__":
+    main()
