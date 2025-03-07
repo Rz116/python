7b56e900afc61036e4594a441929ba8367639885
Rz116 <147172188+Rz116@users.noreply.github.com>
2025-03-07 10:12:57 -0500

    Update tutorial_1.py

--- tutorial_1.py
+++ tutorial_1.py
@@ -1,7 +1,58 @@
+import sys
 import os.path
 from os import path
-def login():
-    getinfo()
+def user():
+    logon = str(input("1.Login \n"
+                                   "2.Sign up \n"
+                                   "Choose an option by typing in 1 or 2: "))
+    length = len(logon)
+    if(length < 1 or length > 1):
+        print("Type in a correct input")
+        user()
+    else:
+        checkinput = ord(logon)
+        if(checkinput < 49 or checkinput > 50):
+            print("Type in a correct input")
+            user()
+        else:
+            if(int(logon) == 1):
+                username = str(input("What is your username: "))
+                length2 = len(username)
+                if(length2 < 1):
+                    print("Type in a correct input")
+                    user()
+                else:
+                    login(username)
+            else:
+                signup()
+def login(nameuser):
+    filedir = os.path.dirname(os.path.realpath("__file__"))
+    filename = nameuser + ".doc"
+    fileexist = bool(path.exists(filename))
+    if(fileexist == True):
+        print("Logged on !")
+    else:
+        print("Username doesnt exist")
+        signup()
+def signup():
+    filedir = os.path.dirname(os.path.realpath("__file__"))
+    username = str(input("Create a username: "))
+    filename = username + ".doc"
+                              
+    length = len(username)
+    if(length < 1):
+        print("Type in a correct input")
+        signup()
+    else:
+        fileexist = bool(path.exists(filename))
+        if(fileexist == False):
+            adminfile = open(filename, "x")
+            adminfile.close()
+            print("Login Created")
+            getinfo()
+        else:
+            print("File already exists")
+            
 def getinfo():
     fileDir = os.path.dirname(os.path.realpath("__file__"))
     whichone = str(input("Which Deparment would you like to shop in: \n"
@@ -14,7 +65,8 @@ def getinfo():
                                              "7.Kosher \n"
                                              "8. Halal \n"
                                              "9. Cart \n"
-                                             "Type in a number from 1-9 to select an option: "))
+                                             "10.Exit the program\n"
+                                             "Type in a number from 1-10 to select an option: "))
                                                                    
     match(whichone):
         case "1":
@@ -43,10 +95,12 @@ def getinfo():
             
         case "9":
             filepath = fileDir + "\cart.py"
-            
+        case "10":
+            print("Goodbye")
+            sys.exit()
         case default:
             print("Type in a correct input")
-            main()
+            getinfo()
     
     filenamepath = {
             "__file__":filepath,
@@ -55,10 +109,9 @@ def getinfo():
 
     with open(filepath,"rb") as file:
         exec(compile(file.read(),filepath,"exec"),filenamepath);
-    
+    main()
 def main():
-    print("hello")
-    login()
+    user()
     
 if __name__ == "__main__":
     main()
